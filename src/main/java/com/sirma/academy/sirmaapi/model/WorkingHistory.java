1bad143cdbd8c0659ee235dccef6ce20d8b86ee2
Valya <wpetrowa02@gmail.com>
2023-12-28 21:36:33 +0200

    date format

--- src/main/java/com/sirma/academy/sirmaapi/model/WorkingHistory.java
+++ src/main/java/com/sirma/academy/sirmaapi/model/WorkingHistory.java
@@ -2,9 +2,9 @@ package com.sirma.academy.sirmaapi.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+
 @Entity
 @Table(name = "work_history")
 public class WorkingHistory {
@@ -18,28 +18,25 @@ public class WorkingHistory {
 
     @Column(name = "project_id")
     private Long projectId;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate dateFrom;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "date_from")
+    @JsonFormat(pattern="[yyyy/MM/dd][yyyy-MM-dd][dd/MM/yyyy][dd-MM-yyyy][yyyy:MM:dd]")
+    private LocalDate dateFrom;
+    @JsonFormat(pattern="[yyyy/MM/dd][yyyy-MM-dd][dd/MM/yyyy][dd-MM-yyyy][yyyy:MM:dd]")
+    @Column(name = "date_to")
     private LocalDate dateTo;
 
     public WorkingHistory() {
     }
-
     public WorkingHistory(Long projectId, Long employeeId, LocalDate dateFrom, LocalDate dateTo) {
         this.projectId = projectId;
         this.employeeId = employeeId;
         this.dateFrom = dateFrom;
         this.dateTo = dateTo;
     }
-
     public Long getProjectId() {
         return projectId;
     }
-
     public void setProjectId(Long projectId) {
         this.projectId = projectId;
     }
@@ -52,19 +49,13 @@ public class WorkingHistory {
         this.employeeId = employeeId;
     }
 
-    public LocalDate getDateFrom() {
-        return dateFrom;
-    }
-
-    public void setDateFrom(LocalDate dateFrom) {
-        this.dateFrom = dateFrom;
-    }
-
     public LocalDate getDateTo() {
         return dateTo;
     }
-
     public void setDateTo(LocalDate dateTo) {
         this.dateTo = dateTo;
+    }
+    public LocalDate getDateFrom() {
+        return dateFrom;
     }
 }
